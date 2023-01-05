07cdb098c9668fa376af20fe06172ca6bcdf99b6
ChoiDongSoo23 <121947056+ChoiDongSoo23@users.noreply.github.com>
2023-01-05 16:33:06 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(function(){
+$(".special img").hover(function(){
+$(".special img").slideDown();
+});
+});
+
+$('.filtering').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+  
+  var filtered = false;
+  
+  $('.js-filter').on('click', function(){
+    if (filtered === false) {
+      $('.filtering').slick('slickFilter',':even');
+      $(this).text('Unfilter Slides');
+      filtered = true;
+    } else {
+      $('.filtering').slick('slickUnfilter');
+      $(this).text('Filter Slides');
+      filtered = false;
+    }
+  });
+ $(function(){
+$(".rightimg img").cover(function(){
+$(".right img img").show()
+});
+ });
+ 
